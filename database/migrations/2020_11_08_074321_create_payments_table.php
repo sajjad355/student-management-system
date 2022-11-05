676dfea888c9abc6c4df8b5ee3418848605f2467
sajjad <sajjadurrahman3434@gmail.com>
2022-11-05 16:35:29 +0600

    feat: deployed the final version

---
+++ database/migrations/2020_11_08_074321_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Type');
+            $table->integer('Total');
+            $table->integer('Paid');
+            $table->integer('Due');  
+            $table->unsignedBigInteger('studentid');
+            $table->foreign('studentid')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,62 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/',"StudentController@index") ;
-Route::get('/home',"StudentController@home") ;
+Route::get('/login1',"StudentController@index2") ->middleware('statusChecker') ;
+Route::get('/',"StudentController@index")->middleware('statusChecker')  ;
+Route::get('/logoutt',"StudentController@logoutt") ;
+Route::get('/notice1',"NoticeController@index1");
+Route::get('/notice',"StudentController@notice") ->middleware('statusChecker') ;
+Route::get('/report',"StudentController@home") ->middleware('statusChecker') ;
 Route::get('/login',"StudentController@login") ;
-Route::get('/batch',"BatchController@index") ;
-Route::get('/createbatch',"BatchController@create") ;
-Route::get('/edit/{id}',"StudentController@edit") ;
-Route::get('/editbatch/{id}',"BatchController@edit") ;
-Route::get('/show/{id}',"StudentController@show") ;
-Route::get('/showbatch/{id}',"BatchController@show") ;
-Route::get('/create',"StudentController@create") ;
-Route::post('/search',"StudentController@search") ;
-Route::post('/store',"StudentController@store") ;
-Route::post('/batchstore',"BatchController@store") ;
-Route::post('/checklogin',"StudentController@checklogin") ;
-Route::post('/update/{id}',"StudentController@update") ;
-Route::post('/updatebatch/{id}',"BatchController@update") ;
-Route::get('/destroy/{id}',"StudentController@destroy") ;
-Route::get('/destroybatch/{id}',"BatchController@destroy") ;
+Route::get('/batch',"BatchController@index")->middleware('statusChecker') ; 
+Route::get('/createbatch',"BatchController@create")->middleware('statusChecker') ;
+Route::get('/edit/{id}',"StudentController@edit") ->middleware('statusChecker') ;
+Route::get('/editPayment/{id}',"StudentController@editPayment") ->middleware('statusChecker') ;
 
+Route::get('/editnotice/{id}',"StudentController@editnotice") ->middleware('statusChecker') ;
+Route::get('/editbatch/{id}',"BatchController@edit")->middleware('statusChecker') ;
+Route::get('/show/{id}',"StudentController@show")->middleware('statusChecker') ;
+Route::get('/Profile',"StudentController@Profile") ->middleware('statusChecker') ;
+Route::get('/showbatch/{id}',"BatchController@show")->middleware('statusChecker') ;
+Route::get('/showstudents/{id}',"BatchController@showstudents") ->middleware('statusChecker');
+Route::get('/create',"StudentController@create") ->middleware('statusChecker') ;
+Route::get('/createnotice',"StudentController@createnotice")->middleware('statusChecker') ;
+Route::post('/search',"StudentController@search")->middleware('statusChecker') ;
+Route::post('/store',"StudentController@store") ->middleware('statusChecker') ;
+Route::post('/storenotice',"StudentController@storenotice")->middleware('statusChecker')  ;
+Route::post('/MI',"StudentController@income")->middleware('statusChecker')  ;
+
+Route::post('/batchstore',"BatchController@store")->middleware('statusChecker') ;
+Route::post('/paymentstore',"studentController@paymentstore") ->middleware('statusChecker') ;
+Route::post('/checklogin',"StudentController@checklogin")->middleware('statusChecker')  ;
+Route::post('/update/{id}',"StudentController@update") ->middleware('statusChecker') ;
+Route::post('/updatePayment/{id}',"StudentController@updatePayment") ->middleware('statusChecker') ;
+Route::post('/updatenotice/{id}',"StudentController@updatenotice") ->middleware('statusChecker') ;
+Route::post('/updateprofile',"StudentController@updateprofile") ->middleware('statusChecker') ;
+Route::post('/updatepassword',"StudentController@updatepassword") ->middleware('statusChecker') ;
+Route::get('/changePassword',"StudentController@changePassword") ->middleware('statusChecker') ;
+
+Route::get('/createpayment/{id}',"StudentController@newpayment") ->middleware('statusChecker') ;
+Route::get('/deletehistory/{id}',"StudentController@deletehistory")->middleware('statusChecker')  ;
+
+Route::post('/updatebatch/{id}',"BatchController@update") ->middleware('statusChecker');
+Route::get('/destroy/{id}',"StudentController@destroy") ->middleware('statusChecker') ;
+Route::get('/destroy1/{bid}/{id}',"StudentController@destroy1") ->middleware('statusChecker') ;
+Route::get('/destroynotice/{id}',"StudentController@destroynotice")->middleware('statusChecker')  ;
+Route::get('/payment/{id}',"StudentController@payment") ->middleware('statusChecker') ;
+Route::get('/destroybatch/{id}',"BatchController@destroy") ->middleware('statusChecker');
+
+
+Route::get('/student',"StudentViewController@index");
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
